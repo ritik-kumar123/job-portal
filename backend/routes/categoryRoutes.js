3f5f304b61dfcc3709ee3dc6d5b943bf58c0e11c
ritik kumar <148605032+ritik-kumar123@users.noreply.github.com>
2025-07-25 09:29:21 +0530

    Update categoryRoutes.js

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { addCategory, deleteCategory, getCategories, } from "../controllers/categoryController.js";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
-// import { isAdmin } from "../middlewares/isAdmin.js";
 import { upload } from "../middlewares/multer.js";
 import { authorizeRoles } from "../middlewares/authorizedRoles.js";
 const categoryRouter = express.Router();
