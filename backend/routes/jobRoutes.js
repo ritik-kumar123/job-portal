c9f701fea9296cb50222271f677928ff9b0b9980
ritik kumar <148605032+ritik-kumar123@users.noreply.github.com>
2025-07-25 09:26:32 +0530

    Update jobRoutes.js

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -3,11 +3,11 @@ import { isAuthenticated } from "../middlewares/isAuthenticated.js";
 import { deleteJob, getAllJobs, getEmployerJobs, postJob } from "../controllers/jobController.js";
 import { authorizeRoles } from "../middlewares/authorizedRoles.js";
 
-const jobRouter= express.Router();
+const jobRouter = express.Router();
 
-jobRouter.post("/post", isAuthenticated,authorizeRoles("employer"), postJob);
-jobRouter.get("/employer-jobs", isAuthenticated,authorizeRoles("employer"), getEmployerJobs);
+jobRouter.post("/post", isAuthenticated, authorizeRoles("employer"), postJob);
+jobRouter.get("/employer-jobs", isAuthenticated, authorizeRoles("employer"), getEmployerJobs);
 jobRouter.get("/all", getAllJobs);
-jobRouter.delete("/delete/:id", isAuthenticated,authorizeRoles("employer,admin"),deleteJob);
+jobRouter.delete("/delete/:id", isAuthenticated, authorizeRoles("employer", "admin"), deleteJob);
 
 export default jobRouter;
