f1ea75b5e2c99bbf4de0dedd5b22b19fe2a00a93
ritik kumar <148605032+ritik-kumar123@users.noreply.github.com>
2025-07-25 09:28:33 +0530

    Update companyRoutes.js

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { addCompany, deleteCompany, getAllCompanies, getEmployerCompanies } from "../controllers/companyController.js";
+import { 
+  addCompany, 
+  deleteCompany, 
+  getAllCompanies, 
+  getEmployerCompanies 
+} from "../controllers/companyController.js";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
-// import { isAdmin } from "../middlewares/isAdmin.js";
 import { upload } from "../middlewares/multer.js";
 import { authorizeRoles } from "../middlewares/authorizedRoles.js";
+
 const companyRouter = express.Router();
 
-companyRouter.post("/add", isAuthenticated,upload.single("logo"), addCompany);
-companyRouter.get("/get-employer-companies", isAuthenticated, getEmployerCompanies);
-companyRouter.get("/all",isAuthenticated,getAllCompanies);
-companyRouter.delete("/delete/:id", isAuthenticated,authorizeRoles("employer"),deleteCompany);
+companyRouter.post("/add", isAuthenticated, authorizeRoles("employer"), upload.single("logo"), addCompany);
+companyRouter.get("/get-employer-companies", isAuthenticated, authorizeRoles("employer"), getEmployerCompanies);
+companyRouter.get("/all", isAuthenticated, getAllCompanies);
+companyRouter.delete("/delete/:id", isAuthenticated, authorizeRoles("employer", "admin"), deleteCompany);
+
 export default companyRouter;
